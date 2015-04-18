d0a335b1ecabfb01c5c6bbc58ca88d789461a7da
alex.podobed <alex.podobed@gmail.com>
2015-04-18 23:04:20 +0200

    added validation, custom checkboxes

--- src/app/loginpage/loginpage.js
+++ src/app/loginpage/loginpage.js
@@ -3,4 +3,16 @@
 angular.module('prototype')
   .controller('LoginCtrl', function ($scope) {
     console.log('login ctrl')
-  });
+
+        $scope.user = {
+            name: "",
+            password: ""
+        }
+
+
+        var form = {"$error": {"required": [
+            {"$viewValue": "", "$modelValue": "", "$validators": {}, "$asyncValidators": {}, "$parsers": [], "$formatters": [null], "$viewChangeListeners": [], "$untouched": true, "$touched": false, "$pristine": true, "$dirty": false, "$valid": false, "$invalid": true, "$error": {"required": true}, "$name": "", "$options": null},
+            {"$viewValue": "", "$modelValue": "", "$validators": {}, "$asyncValidators": {}, "$parsers": [], "$formatters": [null], "$viewChangeListeners": [], "$untouched": true, "$touched": false, "$pristine": true, "$dirty": false, "$valid": false, "$invalid": true, "$error": {"required": true}, "$name": "", "$options": null}
+        ]}, "$name": "loginForm", "$dirty": false, "$pristine": true, "$valid": false, "$invalid": true, "$submitted": false}
+
+    });
