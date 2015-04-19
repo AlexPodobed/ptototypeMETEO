abdf91ac255d300031c562687fd3f0f15b497ea6
alex.podobed <alex.podobed@gmail.com>
2015-04-19 17:40:12 +0200

    small fixes

--- src/app/loginpage/loginpage.js
+++ src/app/loginpage/loginpage.js
@@ -7,12 +7,5 @@ angular.module('prototype')
         $scope.user = {
             name: "",
             password: ""
-        }
-
-
-        var form = {"$error": {"required": [
-            {"$viewValue": "", "$modelValue": "", "$validators": {}, "$asyncValidators": {}, "$parsers": [], "$formatters": [null], "$viewChangeListeners": [], "$untouched": true, "$touched": false, "$pristine": true, "$dirty": false, "$valid": false, "$invalid": true, "$error": {"required": true}, "$name": "", "$options": null},
-            {"$viewValue": "", "$modelValue": "", "$validators": {}, "$asyncValidators": {}, "$parsers": [], "$formatters": [null], "$viewChangeListeners": [], "$untouched": true, "$touched": false, "$pristine": true, "$dirty": false, "$valid": false, "$invalid": true, "$error": {"required": true}, "$name": "", "$options": null}
-        ]}, "$name": "loginForm", "$dirty": false, "$pristine": true, "$valid": false, "$invalid": true, "$submitted": false}
-
+        };
     });
